1a74d2d9fb258e9e9b988059dbd33b5280393d22
fathan <fthimus@gmail.com>
2026-06-25 01:39:34 +0700

    first html

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello world</h1");
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
